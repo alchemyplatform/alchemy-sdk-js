a37e35b661ef269e7f0c07a568eb0c431015619c
Brian Chen <brian@alchemy.com>
2022-06-13 22:20:50 -0500

    Fix dist bundle for node when using imports

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import pkg from './package.json';
 import typescriptPlugin from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 const allBuilds = {
   input: 'src/index.ts',
@@ -14,13 +16,22 @@ const allBuilds = {
       file: pkg.module,
       format: 'esm',
       sourcemap: true
+    },
+    {
+      file: pkg['main-esm'],
+      format: 'es',
+      sourcemap: true
     }
   ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    '@ethersproject/providers/lib/base-provider'
-  ],
-  plugins: [typescriptPlugin(), terser()]
+  external: [...Object.keys(pkg.dependencies || {})],
+  plugins: [
+    typescriptPlugin(),
+    terser(),
+
+    // Needed to resolve `Event` class from Ethers in AlchemyWebSocketProvider
+    nodeResolve({ preferBuiltins: true }),
+    commonjs()
+  ]
 };
 
 export default allBuilds;
