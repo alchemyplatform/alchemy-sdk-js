adf4af90104f7a926cb71ccab1339bd85fb80fb9
David Philipson <david.philipson@gmail.com>
2022-08-24 16:34:15 -0700

    Cleanup and generalization

--- rollup.config.js
+++ rollup.config.js
@@ -3,37 +3,46 @@ import typescriptPlugin from 'rollup-plugin-typescript2';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
+const TRUE_ROOT = 'index';
+const roots = [TRUE_ROOT, 'api/utils'];
+const formats = ['cjs', 'esm', 'es'];
+const plugins = [typescriptPlugin(), nodeResolve(), commonjs()];
+
+// e.g. dirFromPath("foo/bar/baz") -> "foo/bar"
+function dirFromPath(path) {
+  const index = path.lastIndexOf('/');
+  return index === -1 ? '' : path.slice(0, index);
+}
+
+// e.g. lastPartOfPath("foo/bar/baz") -> "baz"
+function lastPartOfPath(path) {
+  const index = path.lastIndexOf('/');
+  return path.slice(index + 1);
+}
+
+// Returns true for any dependency listed in package.json and for any member of
+// roots other than index.js, where roots is the array defined above.
 const isExternalModule = (() => {
-  const deps = new Set(Object.keys(pkg.dependencies));
-  return id => (id.startsWith('.') && id.endsWith('/utils')) || deps.has(id);
+  const deps = new Set(Object.keys(pkg.dependencies ?? {}));
+  const subrootEndings = new Set(
+    roots.filter(root => root !== TRUE_ROOT).map(lastPartOfPath)
+  );
+  return id =>
+    deps.has(id) ||
+    (id.startsWith('.') && subrootEndings.has(lastPartOfPath(id)));
 })();
 
-function makeConfig(input, output) {
+function makeConfig(root) {
   return {
-    input,
-    output: [
-      {
-        dir: `dist/cjs/${output}`,
-        format: 'cjs',
-        sourcemap: true
-      },
-      {
-        dir: `dist/esm/${output}`,
-        format: 'esm',
-        sourcemap: true
-      },
-      {
-        dir: `dist/es/${output}`,
-        format: 'es',
-        sourcemap: true
-      }
-    ],
+    input: `src/${root}.ts`,
+    output: formats.map(format => ({
+      dir: `dist/${format}/${dirFromPath(root)}`,
+      format,
+      sourcemap: true
+    })),
     external: isExternalModule,
-    plugins: [typescriptPlugin(), nodeResolve(), commonjs()]
+    plugins
   };
 }
 
-export default [
-  makeConfig('src/index.ts', ''),
-  makeConfig('src/api/utils.ts', 'api/')
-];
+export default roots.map(makeConfig);
