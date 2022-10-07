cbf2606209237ce962bb5f37893af28a24fc81f1
Brian Chen <brian@alchemy.com>
2022-10-07 12:01:57 -0700

    Make gas optimized txns private (#142)

--- test/integration/integration.test.ts
+++ test/integration/integration.test.ts
@@ -4,10 +4,9 @@ import {
   Network,
   NftExcludeFilters,
   NftTokenType,
-  TokenBalanceType,
-  Wallet
+  TokenBalanceType
 } from '../../src';
-import { Deferred, TESTING_PRIVATE_KEY } from '../test-util';
+import { Deferred } from '../test-util';
 
 /** Temporary test */
 // TODO: REMOVE these tests once we have more comprehensive unit testing.
@@ -285,36 +284,37 @@ describe('E2E integration tests', () => {
     expect(result.contractAddress).toBeDefined();
   });
 
-  describe('sendGasOptimizedTransaction()', () => {
-    it('can send signed transactions', async () => {
-      const response = await alchemy.transact.sendGasOptimizedTransaction([
-        '0x02f86f0107847735940085174876e8008252089415fdad99a4b0c72fc5c2761542e1b17cff7357de843b9aca0080c080a0db73b4c108202f2a0d22d8836ccb2052b24aaffbcbd8222e2030d0b8d68c3218a01ec636ff8a3a6125ba524d72348e91c96be5ac8330eadab31e70e753fbe00236'
-      ]);
-      const tx = await alchemy.transact.getTransaction(
-        response.transactionHashes[0]
-      );
-      expect(tx).toBeDefined();
-      console.log(response);
-    });
+  // TODO: Enable integration tests once method is public.
+  // describe('sendGasOptimizedTransaction()', () => {
+  // it('can send signed transactions', async () => {
+  //   const response = await alchemy.transact.sendGasOptimizedTransaction([
+  //     '0x02f86f0107847735940085174876e8008252089415fdad99a4b0c72fc5c2761542e1b17cff7357de843b9aca0080c080a0db73b4c108202f2a0d22d8836ccb2052b24aaffbcbd8222e2030d0b8d68c3218a01ec636ff8a3a6125ba524d72348e91c96be5ac8330eadab31e70e753fbe00236'
+  //   ]);
+  //   const tx = await alchemy.transact.getTransaction(
+  //     response.transactionHashes[0]
+  //   );
+  //   expect(tx).toBeDefined();
+  //   console.log(response);
+  // });
 
-    it('can send a raw transaction', async () => {
-      const transaction = {
-        to: '0xa238b6008Bc2FBd9E386A5d4784511980cE504Cd',
-        value: 10000,
-        nonce: 2,
-        type: 2,
-        chainId: 5
-      };
-
-      const wallet = new Wallet(TESTING_PRIVATE_KEY);
-      const response = await alchemy.transact.sendGasOptimizedTransaction(
-        transaction,
-        wallet
-      );
-      expect(typeof response.transactionJobId).toEqual('string');
-      expect(response.transactionHashes.length).toEqual(5);
-    });
-  });
+  //   it('can send a raw transaction', async () => {
+  //     const transaction = {
+  //       to: '0xa238b6008Bc2FBd9E386A5d4784511980cE504Cd',
+  //       value: 10000,
+  //       nonce: 2,
+  //       type: 2,
+  //       chainId: 5
+  //     };
+  //
+  //     const wallet = new Wallet(TESTING_PRIVATE_KEY);
+  //     const response = await alchemy.transact.sendGasOptimizedTransaction(
+  //       transaction,
+  //       wallet
+  //     );
+  //     expect(typeof response.transactionJobId).toEqual('string');
+  //     expect(response.transactionHashes.length).toEqual(5);
+  //   });
+  // });
 
   describe('README examples', () => {
     it('Example 1: Getting the Nfts owned by an address', async () => {
