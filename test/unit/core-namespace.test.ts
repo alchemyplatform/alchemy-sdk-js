3c828bdf23be14414789db9eca878499a752d9c6
Sahil Aujla <sahilaujla15@gmail.com>
2023-06-14 09:11:13 -0400

    add isContractAddress to check for contract vs EOA addresses

--- test/unit/core-namespace.test.ts
+++ test/unit/core-namespace.test.ts
@@ -5,12 +5,29 @@ describe('Core Namespace', () => {
   beforeEach(() => {
     alchemy = new Alchemy();
   });
+
   describe('getTokenBalances()', () => {
     it('validates inputs', async () => {
       const address = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045';
       await expect(() =>
         alchemy.core.getTokenBalances(address, [])
       ).rejects.toThrow();
+    });
+  });
+
+  describe('isContractAddress()', () => {
+    it('checks if address is a contract', async () => {
+      const contractAddress = '0x00000000006c3852cbEf3e08E8dF289169EdE581'; // Seaport contract address
+      const result = await alchemy.core.isContractAddress(contractAddress);
+
+      expect(result).toBe(true);
+    });
+
+    it('checks if address is an EOA', async () => {
+      const eoaAddress = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'; // Vitalik's address
+      const result = await alchemy.core.isContractAddress(eoaAddress);
+
+      expect(result).toBe(false);
     });
   });
 });
