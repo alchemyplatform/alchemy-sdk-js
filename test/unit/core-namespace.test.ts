399b7ab3fae16490b2bf6dd4e4debd2eb3730771
Brian Chen <brian@alchemy.com>
2023-09-11 10:52:43 -0500

    revert test

--- test/unit/core-namespace.test.ts
+++ test/unit/core-namespace.test.ts
@@ -1,4 +1,4 @@
-import { Alchemy, Network } from '../../src';
+import { Alchemy } from '../../src';
 
 describe('Core Namespace', () => {
   let alchemy: Alchemy;
@@ -13,13 +13,5 @@ describe('Core Namespace', () => {
         alchemy.core.getTokenBalances(address, [])
       ).rejects.toThrow();
     });
-  });
-
-  it('test', async () => {
-    alchemy = new Alchemy({ network: Network.MATIC_MUMBAI });
-    const a = await alchemy.core.getTransactionReceipt(
-      '0xf5f8aa62d97024d3566ecfeb7fd696f621182608dde8b0eee45fa351d2a00ba7'
-    );
-    console.log(a);
   });
 });
