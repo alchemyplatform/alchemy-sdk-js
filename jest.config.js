a75fa102a205edf13a68c1f3eb83872c264bf76a
Meril <me@mpsq.org>
2024-01-24 22:55:44 +0000

    chore: update to latest `axios` version (#405)
    Fixes #397

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios')
+  }
 };
